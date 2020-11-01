2932c4e9a9f134c04d4d1cd6cf333bc5b3e9c0fa
caocheng <1248672461@qq.com>
2020-11-01 22:18:31 +0800

    test

---
+++ disjoint_set/959.cpp
@@ -0,0 +1,31 @@
+/*
+ * @Author: your name
+ * @Date: 2020-11-01 22:14:15
+ * @LastEditTime: 2020-11-01 22:16:11
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit 
+ * @FilePath: \leetcode\disjoint_set\959.cpp
+ */
+#include <iostream>
+#include <set>
+#include <map>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <string>
+#include <vector>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    //freopen("in.txt", "r", stdin);
+    //freopen("out.txt", "w", stdout);
+
+    cout<<"hello world!"<<endl;
+    system("pause");
+    return 0;
+}
